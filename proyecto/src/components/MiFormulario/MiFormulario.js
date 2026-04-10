bd7ab86287cd868f8ed0e6b4940507754a4597fa
jrodriguezcancio <jrodriguezcancio@udesa.edu.ar>
2026-04-10 18:00:23 -0300

    anda

--- proyecto/src/components/MiFormulario/MiFormulario.js
+++ proyecto/src/components/MiFormulario/MiFormulario.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { withRouter } from "react-router-dom";
+
 class MiFormulario extends Component {
     constructor(props) {
         super(props);
--- proyecto/src/components/NavBar/NavBar.js
+++ proyecto/src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 const Navbar = () => (
   <nav>
