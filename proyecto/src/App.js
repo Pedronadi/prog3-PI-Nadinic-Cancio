c898cbb3d4b0957e258938b436aadfd0d3ae669c
Pedro Nadinic <pedronadinic@MacBook-Pro-de-Pedro.local>
2026-04-10 17:54:25 -0300

    Agregue las peliculas

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header/Header";
+import Index from "./components/Index/Index";
 function App() {
   return (
     <div>
       <Header />
-     
+     <Index />
     </div>
   );
 }
---
+++ proyecto/src/components/MovieCard/MovieCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+function MovieCard(props){
+     return(
+        <article className="single-card-movie">
+                <img src={`https://image.tmdb.org/t/p/w500/${props.peli.poster_path}`} className="card-img-top"
+                    alt="..."></img>
+                <div className="cardBody">
+                    <h5 className="card-title">{props.peli.title}</h5>
+                    <p className="card-text">{props.peli.overview}</p>
+                    <a href="movie.html" className="btn btn-primary">Ver más</a>
+                    <a href="" className="btn alert-primary">🩶</a>
+                </div>
+            </article>
+     )
+}
+
+export default MovieCard;
