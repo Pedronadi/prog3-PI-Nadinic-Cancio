cbc007fb73f18dda7b035124057b46d3c6ac4587
Pedro Nadinic <pedronadinic@MacBook-Pro-de-Pedro.local>
2026-04-15 16:12:49 -0300

    El registro

--- proyecto/src/screens/Registro/Registro.js
+++ proyecto/src/screens/Registro/Registro.js
@@ -2,6 +2,26 @@ import React from "react";
 
 export default function Registro() {
     return(
-        <h1>Registro</h1>
+    <React.Fragment>
+          <h2 class="alert alert-primary">Registro</h2>
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Ingresá tu email"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" class="form-control" id="password" placeholder="Ingresá tu contraseña">
+                        </input>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Registrarse</button>
+                </form>
+                <p class="mt-3 text-center">¿Ya tenés cuenta? <a href="login.html">Iniciar sesión</a></p>
+            </div>
+        </div>
+    </React.Fragment>
     )
 }
