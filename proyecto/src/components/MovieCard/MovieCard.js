36f0ea9484bbf6809f2feb5af5035f93106525a5
Pedro Nadinic <pedronadinic@MacBook-Pro-de-Pedro.local>
2026-04-20 13:57:54 -0300

    Hice lo de ver mas y ver menos

--- proyecto/src/components/MovieCard/MovieCard.js
+++ proyecto/src/components/MovieCard/MovieCard.js
@@ -1,21 +1,38 @@
-import React from "react";
-function MovieCard(props){
-     return(
-        <React.Fragment>
+import React, {Component} from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+class MovieCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        mostrarMas: false
+    }
+    }
+    mostrameMas = () =>{
+        this.setState({
+            mostrarMas: !this.state.mostrarMas
+        })
+    }
+    render(){
+        return(
+         <React.Fragment>
         <article className="single-card-movie">
-                <img src={`https://image.tmdb.org/t/p/w500/${props.peli.poster_path}`} className="card-img-top"
+                <img src={`https://image.tmdb.org/t/p/w500/${this.props.peli.poster_path}`} className="card-img-top"
                     alt="..."></img>
                 <div className="cardBody">
-                    <h5 className="card-title">{props.peli.title}</h5>
-
-                    
-                    <p className="card-text">{props.peli.overview}</p>
-                    <a href= {`/detalles/movie/${props.peli.id}`} className="btn btn-primary">Ver más</a>
+                    <h5 className="card-title">{this.props.peli.title}</h5>
+                 <section className={this.state.mostrarMas ? "show" : "hide"}>
+                    <p className="card-text">{this.props.peli.overview}</p>
+                    </section>
+                    <button className="btn btn-primary" onClick={this.mostrameMas}>{this.state.mostrarMas ? "Ver menos": "Ver más"} </button>
                     <a href="" className="btn alert-primary">🩶</a>
+                    
                 </div>
+                <Link to={`/detalles/movie/${this.props.peli.id}`} className="btn btn-primary">Ver detalle</Link>
             </article>
+            
         </React.Fragment> 
-     )
+        )
+    }
 }
-
-export default MovieCard;
+export default MovieCard
--- proyecto/src/components/SerieCard/SerieCard.js
+++ proyecto/src/components/SerieCard/SerieCard.js
@@ -1,17 +1,36 @@
-import React from "react";
-function SerieCard(props){
-     return(
-        <article className="single-card-on-air mb-3">
-                <img src={`https://image.tmdb.org/t/p/w500/${props.serie.poster_path}`} className="card-img-top"
+import React, {Component}from "react";
+import { Link } from "react-router-dom";
+class SerieCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            mostrarMas: false
+        }
+    }
+    mostrameMas = () =>{
+        this.setState({
+            mostrarMas: !this.state.mostrarMas
+        })
+    }
+    render(){
+        return(
+            <article className="single-card-on-air mb-3">
+                <img src={`https://image.tmdb.org/t/p/w500/${this.props.serie.poster_path}`} className="card-img-top"
                     alt="..."></img>
                 <div className="cardBody">
-                    <h5 className="card-title">{props.serie.name}</h5>
-                    <p className="card-text">{props.serie.overview}</p>
-                    <a href={`/detalles/tv/${props.serie.id}`} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Ver más</a>
+                    <h5 className="card-title">{this.props.serie.name}</h5>
+                    <section className={this.state.mostrarMas ? "show" : "hide"}>
+                    <p className="card-text">{this.props.serie.overview}</p>
+                    </section>
+                    <button className="btn btn-primary" onClick={this.mostrameMas}>{this.state.mostrarMas ? "Ver menos": "Ver más"} </button>
                     <a href="" className="btn alert-primary">🩶</a>
                 </div>
+                <Link to={`/detalles/tv/${this.props.serie.id}`} className="btn btn-primary">Ver detalle</Link>
             </article>
         )
 }
+}
 
 export default SerieCard;   
+
+
